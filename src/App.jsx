d082c8290f1990b6167ab2a2e4fe15e278d5ee7d
kuldeep-rathor <kuldeeprathor91010@gmail.com>
2025-05-06 09:29:25 +0530

    About and exchange rate page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
-// import About from "./pages/About";
-// import ExchangeRates from "./pages/ExchangeRates";
+import About from "./pages/About";
+import ExchangeRates from "./pages/ExchangeRates";
 import NotFound from "./pages/NotFound";
 
 function App() {
@@ -11,8 +11,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
-        {/* <Route path="/exchange-rates" element={<ExchangeRates />} /> */}
+        <Route path="/about" element={<About />} />
+        <Route path="/exchange-rates" element={<ExchangeRates />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
